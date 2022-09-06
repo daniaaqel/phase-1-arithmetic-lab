a82b235875118fd5a567ced39af82dfb8fe2b2a8
daniaaqel <dania.aqel@hotmail.com>
2022-09-06 23:04:06 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1=8;
+let num2=7.75;
+let num3=12;
+let num4=3;
+
+
+
+var multiply=num1*num2;
+
+var random=Math.floor(Math.random()) + 1;
+
+var mod = num3/num4;
+
+var max= Math.max(1, 2, 3, 4, 20);
 
 // Write your code here
